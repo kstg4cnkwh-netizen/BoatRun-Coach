3f3ce9062795fefe2e36e6bd92a0007a4ec94bc5
kstg4cnkwh-netizen <kstg4cnkwh@privaterelay.appleid.com>
2026-06-22 09:01:21 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const C = 'coach-v1.01';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.add('./')));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== C).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
